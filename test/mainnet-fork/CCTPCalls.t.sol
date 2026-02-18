sparkdotfi/spark-alm-controller

refactor: standardization cleanup
- same oz-upgradeable commit as the one previously used from the sdai lib

test/mainnet-fork/CCTPCalls.t.sol
@@ -1,33 +1,34 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
-import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+import { IERC20 } from "../../lib/forge-std/src/interfaces/IERC20.sol";
 
 import { ERC20Mock }       from "../../lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import { Base } from "spark-address-registry/Base.sol";
+import { Base } from "../../lib/spark-address-registry/src/Base.sol";
 
-import { PSM3Deploy }       from "spark-psm/deploy/PSM3Deploy.sol";
-import { IPSM3 }            from "spark-psm/src/PSM3.sol";
-import { MockRateProvider } from "spark-psm/test/mocks/MockRateProvider.sol";
+import { PSM3Deploy }       from "../../lib/spark-psm/deploy/PSM3Deploy.sol";
+import { IPSM3 }            from "../../lib/spark-psm/src/PSM3.sol";
+import { MockRateProvider } from "../../lib/spark-psm/test/mocks/MockRateProvider.sol";
 
-import { CCTPBridgeTesting } from "xchain-helpers/testing/bridges/CCTPBridgeTesting.sol";
-import { CCTPForwarder }     from "xchain-helpers/forwarders/CCTPForwarder.sol";
+import { CCTPForwarder }     from "../../lib/xchain-helpers/src/forwarders/CCTPForwarder.sol";
+import { Bridge }            from "../../lib/xchain-helpers/src/testing/Bridge.sol";
+import { DomainHelpers }     from "../../lib/xchain-helpers/src/testing/Domain.sol";
+import { CCTPBridgeTesting } from "../../lib/xchain-helpers/src/testing/bridges/CCTPBridgeTesting.sol";
 
 import { ForeignControllerDeploy } from "../../deploy/ControllerDeploy.sol";
 import { ControllerInstance }      from "../../deploy/ControllerInstance.sol";
-
-import { ForeignControllerInit } from "../../deploy/ForeignControllerInit.sol";
+import { ForeignControllerInit }   from "../../deploy/ForeignControllerInit.sol";
 
 import { ALMProxy }          from "../../src/ALMProxy.sol";
 import { ForeignController } from "../../src/ForeignController.sol";
 import { RateLimits }        from "../../src/RateLimits.sol";
 import { RateLimitHelpers }  from "../../src/RateLimitHelpers.sol";
 
-import "./ForkTestBase.t.sol";
+import { ForkTestBase } from "./ForkTestBase.t.sol";
 
-contract MainnetControllerTransferUSDCToCCTPFailureTests is ForkTestBase {
+contract MainnetController_TransferUSDCToCCTP_FailureTests is ForkTestBase {
 
     function test_transferUSDCToCCTP_reentrancy() external {
         _setControllerEntered();
@@ -160,7 +161,7 @@ contract MainnetControllerTransferUSDCToCCTPFailureTests is ForkTestBase {
 }
 
 // TODO: Figure out finalized structure for this repo/testing structure wise
-contract BaseChainUSDCToCCTPTestBase is ForkTestBase {
+abstract contract BaseChain_USDCToCCTP_TestBase is ForkTestBase {
 
     using DomainHelpers     for *;
     using CCTPBridgeTesting for Bridge;
@@ -312,7 +313,7 @@ contract BaseChainUSDCToCCTPTestBase is ForkTestBase {
 
 }
 
-contract ForeignControllerTransferUSDCToCCTPFailureTests is BaseChainUSDCToCCTPTestBase {
+contract ForeignController_TransferUSDCToCCTP_FailureTests is BaseChain_USDCToCCTP_TestBase {
 
     using DomainHelpers for *;
 
@@ -451,7 +452,7 @@ contract ForeignControllerTransferUSDCToCCTPFailureTests is BaseChainUSDCToCCTPT
 
 }
 
-contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
+contract USDCToCCTP_IntegrationTests is BaseChain_USDCToCCTP_TestBase {
 
     using DomainHelpers     for *;
     using CCTPBridgeTesting for Bridge;

test/mainnet-fork/DaiUsds.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { ForkTestBase } from "./ForkTestBase.t.sol";
 
-contract MainnetControllerSwapUSDSToDAIFailureTests is ForkTestBase {
+contract MainnetController_SwapUSDSToDAI_FailureTests is ForkTestBase {
 
     function test_swapUSDSToDAI_reentrancy() external {
         _setControllerEntered();
@@ -24,7 +24,7 @@ contract MainnetControllerSwapUSDSToDAIFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDSToDAITests is ForkTestBase {
+contract MainnetController_SwapUSDSToDAI_SuccessTests is ForkTestBase {
 
     function test_swapUSDSToDAI() external {
         vm.prank(relayer);
@@ -56,7 +56,7 @@ contract MainnetControllerSwapUSDSToDAITests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapDAIToUSDSFailureTests is ForkTestBase {
+contract MainnetController_SwapDAIToUSDS_FailureTests is ForkTestBase {
 
     function test_swapDAIToUSDS_reentrancy() external {
         _setControllerEntered();
@@ -75,7 +75,7 @@ contract MainnetControllerSwapDAIToUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapDAIToUSDSTests is ForkTestBase {
+contract MainnetController_SwapDAIToUSDS_SuccessTests is ForkTestBase {
 
     function test_swapDAIToUSDS() external {
         deal(address(dai), address(almProxy), 1_000_000e18);

test/mainnet-fork/PsmCalls.t.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { Ethereum } from "../../lib/spark-address-registry/src/Ethereum.sol";
+
+import { RateLimits } from "../../src/RateLimits.sol";
+
+import { ForkTestBase, IPSMLike } from "./ForkTestBase.t.sol";
 
 interface IPSM is IPSMLike {
+
     function buf() external view returns (uint256);
+
     function line() external view returns (uint256);
+
 }
 
-contract MainnetControllerSwapUSDSToUSDCFailureTests is ForkTestBase {
+contract MainnetController_SwapUSDSToUSDC_FailureTests is ForkTestBase {
 
     function test_swapUSDSToUSDC_reentrancy() external {
         _setControllerEntered();
@@ -50,7 +57,7 @@ contract MainnetControllerSwapUSDSToUSDCFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDSToUSDCTests is ForkTestBase {
+contract MainnetController_SwapUSDSToUSDC_SuccessTests is ForkTestBase {
 
     function test_swapUSDSToUSDC() external {
         vm.prank(relayer);
@@ -137,7 +144,7 @@ contract MainnetControllerSwapUSDSToUSDCTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDCToUSDSFailureTests is ForkTestBase {
+contract MainnetController_SwapUSDCToUSDS_FailureTests is ForkTestBase {
 
     function test_swapUSDCToUSDS_reentrancy() external {
         _setControllerEntered();
@@ -214,7 +221,7 @@ contract MainnetControllerSwapUSDCToUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDCToUSDSTests is ForkTestBase {
+contract MainnetController_SwapUSDCToUSDS_SuccessTests is ForkTestBase {
 
     event Fill(uint256 wad);
 

test/mainnet-fork/Superstate.t.sol
@@ -1,22 +1,28 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { ForkTestBase } from "./ForkTestBase.t.sol";
+
+import { Ethereum } from "../../lib/spark-address-registry/src/Ethereum.sol";
+
+import { RateLimitHelpers } from "../../src/RateLimitHelpers.sol";
+import { RateLimits }       from "../../src/RateLimits.sol";
+
+import { ForkTestBase } from "./ForkTestBase.t.sol";
 
 interface IAllowlistV2Like {
+
     function owner() external view returns (address);
+
     function setEntityIdForAddress(uint256 entityId, address account) external;
+
     function setEntityAllowedForFund(uint256 entityId, string memory fundSymbol, bool isAllowed) external;
+
 }
 
-interface ISSRedemptionLike {
-    function calculateUsdcOut(uint256 ustbAmount) external view returns (uint256 usdcOutAmount, uint256 usdPerUstbChainlinkRaw);
-    function calculateUstbIn(uint256 usdcOutAmount) external view returns (uint256 ustbInAmount, uint256 usdPerUstbChainlinkRaw);
-}
-
-contract SuperstateTestBase is ForkTestBase {
+abstract contract Superstate_TestBase is ForkTestBase {
 
     IAllowlistV2Like allowlist = IAllowlistV2Like(0x02f1fA8B196d21c7b733EB2700B825611d8A38E5);
 
@@ -26,7 +32,7 @@ contract SuperstateTestBase is ForkTestBase {
 
 }
 
-contract MainnetControllerSubscribeSuperstateFailureTests is SuperstateTestBase {
+contract MainnetController_SubscribeSuperstate_FailureTests is Superstate_TestBase {
 
     function test_subscribeSuperstate_reentrancy() external {
         _setControllerEntered();
@@ -76,7 +82,7 @@ contract MainnetControllerSubscribeSuperstateFailureTests is SuperstateTestBase 
 
 }
 
-contract MainnetControllerSubscribeSuperstateSuccessTests is SuperstateTestBase {
+contract MainnetController_SubscribeSuperstate_SuccessTests is Superstate_TestBase {
 
     address sweepDestination;
 
@@ -143,7 +149,7 @@ contract MainnetControllerSubscribeSuperstateSuccessTests is SuperstateTestBase 
 
 }
 
-contract MainnetControllerSuperstateE2ETests is SuperstateTestBase {
+contract MainnetController_Superstate_E2ETests is Superstate_TestBase {
 
     address usccDepositAddress = makeAddr("usccDepositAddress");
 

test/mainnet-fork/VaultCalls.t.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { Ethereum } from "../../lib/spark-address-registry/src/Ethereum.sol";
 
-contract MainnetControllerMintUSDSFailureTests is ForkTestBase {
+import { RateLimits } from "../../src/RateLimits.sol";
+
+import { ForkTestBase } from "./ForkTestBase.t.sol";
+
+contract MainnetController_MintUSDS_FailureTests is ForkTestBase {
 
     function test_mintUSDS_reentrancy() external {
         _setControllerEntered();
@@ -43,7 +47,7 @@ contract MainnetControllerMintUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerMintUSDSSuccessTests is ForkTestBase {
+contract MainnetController_MintUSDS_SuccessTests is ForkTestBase {
 
     function test_mintUSDS() external {
         ( uint256 ink, uint256 art ) = dss.vat.urns(ilk, vault);
@@ -109,7 +113,7 @@ contract MainnetControllerMintUSDSSuccessTests is ForkTestBase {
 
 }
 
-contract MainnetControllerBurnUSDSFailureTests is ForkTestBase {
+contract MainnetController_BurnUSDS_FailureTests is ForkTestBase {
 
     function test_burnUSDS_reentrancy() external {
         _setControllerEntered();
@@ -138,7 +142,7 @@ contract MainnetControllerBurnUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerBurnUSDSSuccessTests is ForkTestBase {
+contract MainnetController_BurnUSDS_SuccessTests is ForkTestBase {
 
     function test_burnUSDS() external {
         // Setup

sky-ecosystem/diamond-pau

test/mainnet-fork/CCTPCalls.t.sol
@@ -1,33 +1,34 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
-import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+import { IERC20 } from "../../lib/forge-std/src/interfaces/IERC20.sol";
 
 import { ERC20Mock }       from "../../lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import { Base } from "spark-address-registry/Base.sol";
+import { Base } from "../../lib/spark-address-registry/src/Base.sol";
 
-import { PSM3Deploy }       from "spark-psm/deploy/PSM3Deploy.sol";
-import { IPSM3 }            from "spark-psm/src/PSM3.sol";
-import { MockRateProvider } from "spark-psm/test/mocks/MockRateProvider.sol";
+import { PSM3Deploy }       from "../../lib/spark-psm/deploy/PSM3Deploy.sol";
+import { IPSM3 }            from "../../lib/spark-psm/src/PSM3.sol";
+import { MockRateProvider } from "../../lib/spark-psm/test/mocks/MockRateProvider.sol";
 
-import { CCTPBridgeTesting } from "xchain-helpers/testing/bridges/CCTPBridgeTesting.sol";
-import { CCTPForwarder }     from "xchain-helpers/forwarders/CCTPForwarder.sol";
+import { CCTPForwarder }     from "../../lib/xchain-helpers/src/forwarders/CCTPForwarder.sol";
+import { Bridge }            from "../../lib/xchain-helpers/src/testing/Bridge.sol";
+import { DomainHelpers }     from "../../lib/xchain-helpers/src/testing/Domain.sol";
+import { CCTPBridgeTesting } from "../../lib/xchain-helpers/src/testing/bridges/CCTPBridgeTesting.sol";
 
 import { ForeignControllerDeploy } from "../../deploy/ControllerDeploy.sol";
 import { ControllerInstance }      from "../../deploy/ControllerInstance.sol";
-
-import { ForeignControllerInit } from "../../deploy/ForeignControllerInit.sol";
+import { ForeignControllerInit }   from "../../deploy/ForeignControllerInit.sol";
 
 import { ALMProxy }          from "../../src/ALMProxy.sol";
 import { ForeignController } from "../../src/ForeignController.sol";
 import { RateLimits }        from "../../src/RateLimits.sol";
 import { RateLimitHelpers }  from "../../src/RateLimitHelpers.sol";
 
-import "./ForkTestBase.t.sol";
+import { ForkTestBase } from "./ForkTestBase.t.sol";
 
-contract MainnetControllerTransferUSDCToCCTPFailureTests is ForkTestBase {
+contract MainnetControlle_CCTP_TransferUSDCToCCTP_FailureTests is ForkTestBase {
 
     function test_transferUSDCToCCTP_reentrancy() external {
         _setControllerEntered();
@@ -160,7 +161,7 @@ contract MainnetControllerTransferUSDCToCCTPFailureTests is ForkTestBase {
 }
 
 // TODO: Figure out finalized structure for this repo/testing structure wise
-contract BaseChainUSDCToCCTPTestBase is ForkTestBase {
+abstract contract BaseChain_CCTP_TestBase is ForkTestBase {
 
     using DomainHelpers     for *;
     using CCTPBridgeTesting for Bridge;
@@ -312,7 +313,7 @@ contract BaseChainUSDCToCCTPTestBase is ForkTestBase {
 
 }
 
-contract ForeignControllerTransferUSDCToCCTPFailureTests is BaseChainUSDCToCCTPTestBase {
+contract ForeignController_CCTP_TransferUSDCToCCTP_FailureTests is BaseChain_CCTP_TestBase {
 
     using DomainHelpers for *;
 
@@ -451,7 +452,7 @@ contract ForeignControllerTransferUSDCToCCTPFailureTests is BaseChainUSDCToCCTPT
 
 }
 
-contract USDCToCCTPIntegrationTests is BaseChainUSDCToCCTPTestBase {
+contract CCTP_TransferUSDCToCCTP_IntegrationTests is BaseChain_CCTP_TestBase {
 
     using DomainHelpers     for *;
     using CCTPBridgeTesting for Bridge;

test/mainnet-fork/DaiUsds.t.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { ForkTestBase } from "./ForkTestBase.t.sol";
 
-contract MainnetControllerSwapUSDSToDAIFailureTests is ForkTestBase {
+contract MainnetController_DAIUSDS_SwapUSDSToDAI_FailureTests is ForkTestBase {
 
     function test_swapUSDSToDAI_reentrancy() external {
         _setControllerEntered();
@@ -24,7 +24,7 @@ contract MainnetControllerSwapUSDSToDAIFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDSToDAITests is ForkTestBase {
+contract MainnetController_DAIUSDS_SwapUSDSToDAI_SuccessTests is ForkTestBase {
 
     function test_swapUSDSToDAI() external {
         vm.prank(relayer);
@@ -56,7 +56,7 @@ contract MainnetControllerSwapUSDSToDAITests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapDAIToUSDSFailureTests is ForkTestBase {
+contract MainnetController_DAIUSDS_SwapDAIToUSDS_FailureTests is ForkTestBase {
 
     function test_swapDAIToUSDS_reentrancy() external {
         _setControllerEntered();
@@ -75,7 +75,7 @@ contract MainnetControllerSwapDAIToUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapDAIToUSDSTests is ForkTestBase {
+contract MainnetController_DAIUSDS_SwapDAIToUSDS_SuccessTests is ForkTestBase {
 
     function test_swapDAIToUSDS() external {
         deal(address(dai), address(almProxy), 1_000_000e18);

test/mainnet-fork/PsmCalls.t.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { Ethereum } from "../../lib/spark-address-registry/src/Ethereum.sol";
+
+import { RateLimits } from "../../src/RateLimits.sol";
+
+import { ForkTestBase, IPSMLike } from "./ForkTestBase.t.sol";
 
 interface IPSM is IPSMLike {
+
     function buf() external view returns (uint256);
+
     function line() external view returns (uint256);
+
 }
 
-contract MainnetControllerSwapUSDSToUSDCFailureTests is ForkTestBase {
+contract MainnetController_PSM_SwapUSDSToUSDC_FailureTests is ForkTestBase {
 
     function test_swapUSDSToUSDC_reentrancy() external {
         _setControllerEntered();
@@ -50,7 +57,7 @@ contract MainnetControllerSwapUSDSToUSDCFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDSToUSDCTests is ForkTestBase {
+contract MainnetController_PSM_SwapUSDSToUSDC_SuccessTests is ForkTestBase {
 
     function test_swapUSDSToUSDC() external {
         vm.prank(relayer);
@@ -137,7 +144,7 @@ contract MainnetControllerSwapUSDSToUSDCTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDCToUSDSFailureTests is ForkTestBase {
+contract MainnetController_PSM_SwapUSDCToUSDS_FailureTests is ForkTestBase {
 
     function test_swapUSDCToUSDS_reentrancy() external {
         _setControllerEntered();
@@ -214,7 +221,7 @@ contract MainnetControllerSwapUSDCToUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerSwapUSDCToUSDSTests is ForkTestBase {
+contract MainnetController_PSM_SwapUSDCToUSDS_SuccessTests is ForkTestBase {
 
     event Fill(uint256 wad);
 

test/mainnet-fork/Superstate.t.sol
@@ -1,22 +1,28 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { ForkTestBase } from "./ForkTestBase.t.sol";
+
+import { Ethereum } from "../../lib/spark-address-registry/src/Ethereum.sol";
+
+import { RateLimitHelpers } from "../../src/RateLimitHelpers.sol";
+import { RateLimits }       from "../../src/RateLimits.sol";
+
+import { ForkTestBase } from "./ForkTestBase.t.sol";
 
 interface IAllowlistV2Like {
+
     function owner() external view returns (address);
+
     function setEntityIdForAddress(uint256 entityId, address account) external;
+
     function setEntityAllowedForFund(uint256 entityId, string memory fundSymbol, bool isAllowed) external;
+
 }
 
-interface ISSRedemptionLike {
-    function calculateUsdcOut(uint256 ustbAmount) external view returns (uint256 usdcOutAmount, uint256 usdPerUstbChainlinkRaw);
-    function calculateUstbIn(uint256 usdcOutAmount) external view returns (uint256 ustbInAmount, uint256 usdPerUstbChainlinkRaw);
-}
-
-contract SuperstateTestBase is ForkTestBase {
+abstract contract Superstate_TestBase is ForkTestBase {
 
     IAllowlistV2Like allowlist = IAllowlistV2Like(0x02f1fA8B196d21c7b733EB2700B825611d8A38E5);
 
@@ -26,7 +32,7 @@ contract SuperstateTestBase is ForkTestBase {
 
 }
 
-contract MainnetControllerSubscribeSuperstateFailureTests is SuperstateTestBase {
+contract MainnetController_Superstate_Subscribe_FailureTests is Superstate_TestBase {
 
     function test_subscribeSuperstate_reentrancy() external {
         _setControllerEntered();
@@ -76,7 +82,7 @@ contract MainnetControllerSubscribeSuperstateFailureTests is SuperstateTestBase 
 
 }
 
-contract MainnetControllerSubscribeSuperstateSuccessTests is SuperstateTestBase {
+contract MainnetController_Superstate_Subscribe_SuccessTests is Superstate_TestBase {
 
     address sweepDestination;
 
@@ -143,7 +149,7 @@ contract MainnetControllerSubscribeSuperstateSuccessTests is SuperstateTestBase 
 
 }
 
-contract MainnetControllerSuperstateE2ETests is SuperstateTestBase {
+contract MainnetController_Superstate_E2ETests is Superstate_TestBase {
 
     address usccDepositAddress = makeAddr("usccDepositAddress");
 

test/mainnet-fork/VaultCalls.t.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.21;
 
 import { ReentrancyGuard } from "../../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-import "./ForkTestBase.t.sol";
+import { Ethereum } from "../../lib/spark-address-registry/src/Ethereum.sol";
 
-contract MainnetControllerMintUSDSFailureTests is ForkTestBase {
+import { RateLimits } from "../../src/RateLimits.sol";
+
+import { ForkTestBase } from "./ForkTestBase.t.sol";
+
+contract MainnetController_Vault_MintUSDS_FailureTests is ForkTestBase {
 
     function test_mintUSDS_reentrancy() external {
         _setControllerEntered();
@@ -43,7 +47,7 @@ contract MainnetControllerMintUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerMintUSDSSuccessTests is ForkTestBase {
+contract MainnetController_Vault_MintUSDS_SuccessTests is ForkTestBase {
 
     function test_mintUSDS() external {
         ( uint256 ink, uint256 art ) = dss.vat.urns(ilk, vault);
@@ -109,7 +113,7 @@ contract MainnetControllerMintUSDSSuccessTests is ForkTestBase {
 
 }
 
-contract MainnetControllerBurnUSDSFailureTests is ForkTestBase {
+contract MainnetController_Vault_BurnUSDS_FailureTests is ForkTestBase {
 
     function test_burnUSDS_reentrancy() external {
         _setControllerEntered();
@@ -138,7 +142,7 @@ contract MainnetControllerBurnUSDSFailureTests is ForkTestBase {
 
 }
 
-contract MainnetControllerBurnUSDSSuccessTests is ForkTestBase {
+contract MainnetController_Vault_BurnUSDS_SuccessTests is ForkTestBase {
 
     function test_burnUSDS() external {
         // Setup