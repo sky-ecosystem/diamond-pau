b242bee5196b24f38d8e0f63578fb738ae77ac76
Michael De Luca <michael.deluca@circle-free.com>
2026-02-23 13:57:19 -0500

    fix: PR Review

--- src/libraries/FarmLib.sol
+++ src/libraries/FarmLib.sol
@@ -10,11 +10,11 @@ import { makeAddressKey } from "../RateLimitHelpers.sol";
 
 interface IFarmLike {
 
+    function getReward() external;
+
     function stake(uint256 amount) external;
 
     function withdraw(uint256 amount) external;
-
-    function getReward() external;
 
     function stakingToken() external view returns (address);
 
