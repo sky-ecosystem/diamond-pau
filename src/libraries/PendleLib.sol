bdf5474106ca0c399ae4f761e5c2b99b818ed0c5
Bartek Rutkowski <barrutko.eth@gmail.com>
2026-02-25 19:36:34 +0100

    fix: formatting

--- src/libraries/PendleLib.sol
+++ src/libraries/PendleLib.sol
@@ -41,26 +41,36 @@ struct TokenOutput {
 }
 
 interface IPendleRouter {
+
     function redeemPyToToken(
         address              receiver,
         address              YT,
         uint256              netPyIn,
         TokenOutput calldata output
     ) external returns (uint256 netTokenOut, uint256 netSyInterm);
+
 }
 
 interface IPendleMarket {
+
     function readTokens() external view returns (address _SY, address _PT, address _YT);
+
     function isExpired() external view returns (bool);
+
     function expiry() external view returns (uint256);
+
 }
 
 interface ISY {
+
     function yieldToken() external view returns (address);
+
 }
 
 interface IYT {
+
     function pyIndexCurrent() external returns (uint256);
+
 }
 
 library PendleLib {
