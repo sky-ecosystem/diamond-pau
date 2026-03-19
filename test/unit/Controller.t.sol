80fd019114ab4609a9097d2a99028a6e332eca1a
Michael De Luca <michael.deluca@circle-free.com>
2026-03-19 10:17:58 -0400

    fix: PR Review

--- test/unit/Controller.t.sol
+++ test/unit/Controller.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.34;
 
-import { Test, console2 } from "../../lib/forge-std/src/Test.sol";
+import { Test } from "../../lib/forge-std/src/Test.sol";
 
 import { IAccessControl } from "../../lib/openzeppelin-contracts/contracts/access/IAccessControl.sol";
 
