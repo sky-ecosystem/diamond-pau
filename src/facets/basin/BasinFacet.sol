43f894df3f4a572a650bee0e185b6797ffa97586
iamchrissmith <1408372+iamchrissmith@users.noreply.github.com>
2026-04-01 12:39:03 +0200

    copy psm3 to make initial basin directory

---
+++ src/facets/basin/BasinFacet.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.34;
+
+import { ApproveLib }     from "../../libraries/ApproveLib.sol";
+import { makeAddressKey } from "../../libraries/RateLimitHelpers.sol";
+
+import { IALMProxy }   from "../../interfaces/IALMProxy.sol";
+import { IRateLimits } from "../../interfaces/IRateLimits.sol";
+
+import { FacetBase } from "../FacetBase.sol";
+
+import { IPSM3Facet } from "./IPSM3Facet.sol";
+
+interface IPSM3Like {
+
+    function deposit(address asset, address receiver, uint256 assetsToDeposit)
+        external
+        returns (uint256 newShares);
+
+    function withdraw(address asset, address receiver, uint256 maxAssetsToWithdraw)
+        external
+        returns (uint256 assetsWithdrawn);
+
+}
+
+contract PSM3Facet is IPSM3Facet, FacetBase {
+
+    /**********************************************************************************************/
+    /*** Constants                                                                              ***/
+    /**********************************************************************************************/
+
+    bytes32 public constant override LIMIT_DEPOSIT  = keccak256("LIMIT_PSM_DEPOSIT");
+    bytes32 public constant override LIMIT_WITHDRAW = keccak256("LIMIT_PSM_WITHDRAW");
+
+    /**********************************************************************************************/
+    /*** Declarations                                                                           ***/
+    /**********************************************************************************************/
+
+    address public immutable override psm;
+
+    /**********************************************************************************************/
+    /*** Constructor                                                                            ***/
+    /**********************************************************************************************/
+
+    constructor(address psm_) {
+        psm = psm_;
+    }
+
+    /**********************************************************************************************/
+    /*** External Interactive Relayer Functions                                                 ***/
+    /**********************************************************************************************/
+
+    function deposit(address asset, uint256 amount)
+        external
+        override
+        nonReentrant
+        onlyRole(RELAYER_ROLE)
+        returns (uint256 shares)
+    {
+        _decreaseRateLimit(LIMIT_DEPOSIT, asset, amount);
+
+        address proxy = _getSharedControllerStorage().proxy;
+
+        // Approve `asset` to PSM from the proxy (assumes the proxy has enough `asset`).
+        ApproveLib.approve(asset, proxy, psm, amount);
+
+        // Deposit `amount` of `asset` in the PSM, decode the result to get `shares`.
+        return abi.decode(
+            IALMProxy(proxy).doCall(
+                psm,
+                abi.encodeCall(IPSM3Like.deposit, (asset, proxy, amount))
+            ),
+            (uint256)
+        );
+    }
+
+    function withdraw(address asset, uint256 maxAmount)
+        external
+        override
+        nonReentrant
+        onlyRole(RELAYER_ROLE)
+        returns (uint256 assetsWithdrawn)
+    {
+        address proxy = _getSharedControllerStorage().proxy;
+
+        // Withdraw up to `maxAmount` of `asset` in the PSM, decode the result to get
+        // `assetsWithdrawn` (assumes the proxy has enough PSM shares).
+        // NOTE: Rate limited at end of function, so cannot return here.
+        assetsWithdrawn = abi.decode(
+            IALMProxy(proxy).doCall(
+                psm,
+                abi.encodeCall(IPSM3Like.withdraw, (asset, proxy, maxAmount))
+            ),
+            (uint256)
+        );
+
+        _decreaseRateLimit(LIMIT_WITHDRAW, asset, assetsWithdrawn);
+    }
+
+    /**********************************************************************************************/
+    /*** Internal Interactive Functions                                                         ***/
+    /**********************************************************************************************/
+
+    function _decreaseRateLimit(bytes32 key, address asset, uint256 amount) internal {
+        IRateLimits(_getSharedControllerStorage().rateLimits).triggerRateLimitDecrease(
+            makeAddressKey(key, asset),
+            amount
+        );
+    }
+
+}
---
+++ src/facets/basin/IBasinFacet.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.34;
+
+import { IFacetBase } from "../IFacetBase.sol";
+
+interface IBasinFacet is IFacetBase {
+
+    /**********************************************************************************************/
+    /*** Interactive Functions                                                                  ***/
+    /**********************************************************************************************/
+
+    function deposit(address asset, uint256 amount) external returns (uint256 shares);
+
+    function withdraw(address asset, uint256 maxAmount) external returns (uint256 assetsWithdrawn);
+
+    /**********************************************************************************************/
+    /*** Variables                                                                              ***/
+    /**********************************************************************************************/
+
+    function LIMIT_DEPOSIT() external pure returns (bytes32);
+
+    function LIMIT_WITHDRAW() external pure returns (bytes32);
+
+    function basin() external view returns (address);
+
+}
