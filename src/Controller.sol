b5a1addd42f4af03a46fa8db54efaa120cc8548f
TheMj0ln1r <charan.nomula.cn@gmail.com>
2026-04-03 22:43:02 +0530

    fix: review comments

--- src/Controller.sol
+++ src/Controller.sol
@@ -251,9 +251,9 @@ contract Controller is IController, ControllerSharedStorage, ReentrancyGuard {
     }
 
     function _removeWire(bytes4 callSelector) internal {
-        ControllerStorage storage $ = _getControllerStorage();
-
         _revertIfCallSelectorIsHardcoded(callSelector);
+
+        ControllerStorage storage $ = _getControllerStorage();
 
         Dispatch storage dispatch = $.dispatches[callSelector];
 
