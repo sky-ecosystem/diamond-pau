1c2c15fd703e6c2933cc25b9862e388325d63b81
0xSimon <simonnfts@gmail.com>
2026-04-27 21:43:22 -0400

    feat(nfat): nit spacing

--- src/facets/nfat-halo/NFATHaloFacet.sol
+++ src/facets/nfat-halo/NFATHaloFacet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.34;
 
-import { ApproveLib }                  from "../../libraries/ApproveLib.sol";
+import { ApproveLib }                   from "../../libraries/ApproveLib.sol";
 import { makeAddressAddressUint256Key } from "../../libraries/RateLimitHelpers.sol";
 
 import { IALMProxy }   from "../../interfaces/IALMProxy.sol";
