4745c43dbeb340af5fb54ce72620d7e5431f2ffd
Michael De Luca <michael.deluca@circle-free.com>
2026-09-16 18:11:47 -0400

    fix: remove `_UNISWAP_V4_POOL_MANAGER `

--- test/mainnet-fork/ForkTestBase.t.sol
+++ test/mainnet-fork/ForkTestBase.t.sol
@@ -130,7 +130,6 @@ abstract contract ForkTestBase is DssTest {
 
     // NOTE: From https://docs.uniswap.org/contracts/v4/deployments (Ethereum Mainnet).
     address internal constant _PERMIT2                     = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
-    address internal constant _UNISWAP_V4_POOL_MANAGER     = 0x000000000004444c5dc75cB358380D2e3dE08A90;
     address internal constant _UNISWAP_V4_POSITION_MANAGER = 0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e;
     address internal constant _UNISWAP_V4_ROUTER           = 0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af;
 
